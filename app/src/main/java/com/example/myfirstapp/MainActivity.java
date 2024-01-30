ee5edf8609696c5b20f0cc8c210598c67dd1deca
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-01-30 17:53:45 +0900

    Feat : add spinner

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] items = {"한국어", "english", "中文"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        var items = getResources().getStringArray(R.array.spinner);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, items );
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(0); // 기본값 한국어
 
     }
 }
